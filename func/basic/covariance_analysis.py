27d3f06590c6b0683687b69cb8eebdf7d0c854e3
yumin <rollingbill@nongshim.com>
2025-02-06 10:58:26 +0900

    Add exception for data format

--- func/basic/covariance_analysis.py
+++ func/basic/covariance_analysis.py
@@ -13,9 +13,13 @@ import plotly.graph_objects as go
 def covariance_analysis_set():
     df = convert_to_calculatable_df()
     st.session_state.predictor = st.multiselect("변수", df.columns.tolist())
+    for col in st.session_state.predictor:
+        if df[col].dtype == "object":
+            st.error(f"{col}는 계량형 변수가 아닙니다. 계량형 변수만 선택해주세요.")
+            return
 
 def covariance_analysis_cal(df):
-    pass
+    st.session_state.stats_df = df.cov()
 
 def covariance_analysis_plot(df):
     pass
@@ -25,9 +29,13 @@ def covariance_analysis_run():
     df = df[st.session_state.predictor]
     # --------------------------------------
     if len(st.session_state.predictor)>0:
+        for col in st.session_state.predictor:
+            if df[col].dtype == "object":
+                st.error(f"{col}는 계량형 변수가 아닙니다. 계량형 변수만 선택해주세요.")
+                return
         covariance_analysis_cal(df)
         st.write("**공분산**")
-        st.data_editor(df.cov())
+        st.data_editor(st.session_state.stats_df)
         # --------------------------------------
         covariance_analysis_plot(df)
     else:
